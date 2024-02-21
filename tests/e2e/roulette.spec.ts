7f3651e0a15a2f9666cd108c042f783775bf47eb
alinasytnik <alinasytnik1999@gmail.com>
2024-02-21 20:59:51 +0100

    chore: tests refactored

--- tests/e2e/roulette.spec.ts
+++ tests/e2e/roulette.spec.ts
@@ -1,217 +1,98 @@
-import { test, expect } from "@playwright/test";
-import { isRouletteSpinning } from "../../utils/roulette";
+import { test } from "@playwright/test";
+import { RoulettePage } from "../../utils/roulette";
 
 test.describe("Roulette", () => {
-  const timerSelector = "div.font-numeric.text-2xl.font-bold";
-  const timerVisibleSelector = "div.wheel__item--visible";
-  const betInputSelector = 'input[placeholder="Enter bet amount..."]';
-  const originalValue = 1.23;
+  let roulettePage: RoulettePage;
 
   test.beforeEach(async ({ page }) => {
-    await page.goto("https://csgoempire.com/");
-    await expect(page).toHaveTitle(
-      /CSGOEmpire | The most trusted CSGO Skin Gambling Site/i,
+    roulettePage = new RoulettePage(page);
+    await roulettePage.goto();
+  });
+
+  test("Verifies the roulette isn't spinning while the timer is visible", async () => {
+    await roulettePage.verifyRouletteNotSpinning();
+  });
+
+  test("Verifies the timer is hidden once it reaches 0 sec", async () => {
+    await roulettePage.waitForTimerToReachZero();
+  });
+
+  test("Verifies the sound can be toggled on and off", async () => {
+    await roulettePage.toggleSound(["Sound off", "Sound on"]);
+  });
+
+  test("Verifies that the total number of all winning bets is 100", async () => {
+    await roulettePage.verifyTotalWinsEquals100();
+  });
+
+  test("Verifies that the system accepts the input and reflects the correct amount in the field", async () => {
+    await roulettePage.verifyInputReflection("100");
+  });
+
+  test("Verifies the “Clear” button resets the entered number to zero", async () => {
+    await roulettePage.verifyInputReflection("123");
+    await roulettePage.verifyButtonFunctionality("Clear", 123, "0");
+  });
+
+  test("Verifies button functionality adds the correct amount", async () => {
+    const originalValue = 1.26;
+
+    //"Verifies the “+0.01” button adds 0.01 to the number entered in the field"
+    await roulettePage.verifyButtonFunctionality(
+      "+ 0.01",
+      originalValue,
+      (originalValue + 0.01).toFixed(2),
+    );
+
+    //"Verifies the “+0.1” button adds 0.1 to the number entered in the field"
+    await roulettePage.verifyButtonFunctionality(
+      "+ 0.1",
+      originalValue,
+      (originalValue + 0.1).toFixed(2),
+    );
+
+    //"Verifies the “+1” button adds 1 to the number entered in the field"
+    await roulettePage.verifyPlusOneButtonFunctionality(
+      "+ 1",
+      originalValue,
+      (originalValue + 1).toFixed(2),
+    );
+
+    //"Verifies the “+10” button adds 10 to the number entered in the field"
+    await roulettePage.verifyPlusOneButtonFunctionality(
+      "+ 10",
+      originalValue,
+      (originalValue + 10).toFixed(2),
+    );
+
+    //"Verifies the “+100” button adds 100 to the number entered in the field"
+    await roulettePage.verifyButtonFunctionality(
+      "+ 100",
+      originalValue,
+      (originalValue + 100).toFixed(2),
+    );
+
+    //"Verifies the “1/2” button divides the number entered in the field by two"
+    await roulettePage.verifyButtonFunctionality(
+      "1/ 2",
+      originalValue,
+      (originalValue / 2).toFixed(2),
+    );
+
+    //Verifies the “x2” button multiplies the number entered in the field by two
+    await roulettePage.verifyButtonFunctionality(
+      "x 2",
+      originalValue,
+      (originalValue * 2).toFixed(2),
     );
   });
 
-  test("Verifies the roulette isn't spinning while the timer is visible", async ({
-    page,
-  }) => {
-    await expect(page.locator(timerSelector)).toBeVisible();
-
-    const rouletteSpinning = await isRouletteSpinning(page);
-    expect(rouletteSpinning).toBeFalsy();
+  test("Verifies the display and availability of all three betting options while the roulette is idle and the betting timer is active", async () => {
+    await roulettePage.verifyBettingOptionsAvailability();
   });
 
-  test("Verifies the timer is hidden once it reaches 0 sec", async ({
-    page,
-  }) => {
-    await expect(page.locator(timerSelector)).toBeVisible();
-
-    await page.waitForFunction((selector: string) => {
-      const timer: Element | null = document.querySelector(selector);
-      return timer && timer.textContent?.trim() === "0.00";
-    }, timerSelector);
-
-    // Note: After the spin, only one `timerVisibleSelector` is visible
-    await expect(page.locator(timerVisibleSelector)).toHaveCount(1);
-  });
-
-  test("Verifies the sound can be toggled on and off", async ({ page }) => {
-    // No choice to pick it up in other way. Otherwise I would add a test ID
-    const soundToggleButtonSelector =
-      '//*[@id="app"]/div[1]/div[2]/div/div/div[1]/div[2]/div/div[1]/button';
-
-    await page.click(soundToggleButtonSelector);
-
-    await expect(page.locator(soundToggleButtonSelector)).toHaveText(
-      "Sound off",
-    );
-
-    await page.click(soundToggleButtonSelector);
-
-    await expect(page.locator(soundToggleButtonSelector)).toHaveText(
-      "Sound on",
-    );
-  });
-
-  test("Verifies that the total number of all winning bets is 100", async ({
-    page,
-  }) => {
-    const getNumberNextToCoinClass = (className: string) =>
-      page.evaluate((className) => {
-        const targetElement = document.querySelector(`.${className}.h-16.w-16`);
-        const numberElement = targetElement?.nextElementSibling;
-        return numberElement ? numberElement.textContent : null;
-      }, className);
-
-    const coinCTnumberOfWinsText = await getNumberNextToCoinClass("coin-ct");
-    const coinBonusNumberOfWinsText =
-      await getNumberNextToCoinClass("coin-bonus");
-    const coinTnumberOfWinsText = await getNumberNextToCoinClass("coin-t");
-
-    const tWinsNumber = parseInt(coinTnumberOfWinsText || "0", 10);
-    const ctWinsNumber = parseInt(coinCTnumberOfWinsText || "0", 10);
-    const bonusWinsNumber = parseInt(coinBonusNumberOfWinsText || "0", 10);
-
-    const totalWins = tWinsNumber + ctWinsNumber + bonusWinsNumber;
-
-    expect(totalWins).toBe(100);
-  });
-
-  test("Verifies that the system accepts the input and reflects the correct amount in the field", async ({
-    page,
-  }) => {
-    const amountToInput = "100";
-
-    await page.fill(betInputSelector, amountToInput);
-
-    await expect(page.locator(betInputSelector)).toHaveValue(amountToInput);
-  });
-  test("Verifies the “Clear” button resets the entered number to zero", async ({
-    page,
-  }) => {
-    await page.fill(betInputSelector, "123");
-
-    await page.getByRole("button", { name: "Clear" }).first().click();
-
-    await expect(page.locator(betInputSelector)).toHaveValue("0");
-  });
-
-  test("Verifies the “+0.01” button adds 0.01 to the number entered in the field", async ({
-    page,
-  }) => {
-    await page.fill(betInputSelector, originalValue.toString());
-
-    await page.getByRole("button", { name: "+ 0.01" }).click();
-
-    const expectedValue = (originalValue + 0.01).toFixed(2);
-    await expect(page.locator(betInputSelector)).toHaveValue(expectedValue);
-  });
-  test("Verifies the “+0.1” button adds 0.1 to the number entered in the field", async ({
-    page,
-  }) => {
-    await page.fill(betInputSelector, originalValue.toString());
-
-    await page.getByRole("button", { name: "+ 0.1" }).click();
-
-    const expectedValue = (originalValue + 0.1).toFixed(2);
-    await expect(page.locator(betInputSelector)).toHaveValue(expectedValue);
-  });
-  test("Verifies the “+1” button adds 1 to the number entered in the field", async ({
-    page,
-  }) => {
-    await page.fill(betInputSelector, originalValue.toString());
-
-    await page.getByRole("button", { name: "+ 1" }).first().click();
-
-    const expectedValue = (originalValue + 1).toFixed(2);
-    await expect(page.locator(betInputSelector)).toHaveValue(expectedValue);
-  });
-  test("Verifies the “+10” button adds 10 to the number entered in the field", async ({
-    page,
-  }) => {
-    await page.fill(betInputSelector, originalValue.toString());
-
-    // Isn't picking up by text
-    await page.getByRole("button", { name: "+ 10" }).first().click();
-
-    const expectedValue = (originalValue + 10).toFixed(2);
-    await expect(page.locator(betInputSelector)).toHaveValue(expectedValue);
-  });
-  test("Verifies the “+100” button adds 100 to the number entered in the field", async ({
-    page,
-  }) => {
-    await page.fill(betInputSelector, originalValue.toString());
-
-    await page.getByRole("button", { name: "+ 100" }).first().click();
-
-    const expectedValue = (originalValue + 100).toFixed(2);
-    await expect(page.locator(betInputSelector)).toHaveValue(expectedValue);
-  });
-  test("Verifies the “1/2” button divides the number entered in the field by two", async ({
-    page,
-  }) => {
-    const originalValue = 10;
-    await page.fill(betInputSelector, originalValue.toString());
-
-    await page.getByRole("button", { name: "1/ 2" }).click();
-
-    const expectedValue = (originalValue / 2).toString();
-    await expect(page.locator(betInputSelector)).toHaveValue(expectedValue);
-  });
-  test("Verifies the “x2” button multiplies the number entered in the field by two", async ({
-    page,
-  }) => {
-    const originalValue = 10;
-    await page.fill(betInputSelector, originalValue.toString());
-
-    await page.getByRole("button", { name: "x 2" }).click();
-
-    const expectedValue = (originalValue * 2).toString();
-    await expect(page.locator(betInputSelector)).toHaveValue(expectedValue);
-  });
-
-  test("Verifies the display and availability of all three betting options while the roulette is idle and the betting timer is active", async ({
-    page,
-  }) => {
+  test("Verifies the Sign In modal is shown if not authenticated user trying to place a bet", async () => {
     test.setTimeout(30000);
-    const bettingOptions = page.locator("button.bet-btn");
-    await expect(bettingOptions).toHaveCount(3);
-
-    await bettingOptions.first().isEnabled();
-    await bettingOptions.nth(1).isEnabled();
-    await bettingOptions.nth(2).isEnabled();
+    await roulettePage.verifySignInModalOnBet();
   });
-
-  test("Verifies the Sign In modal is shown if not authenticated user trying to place a bet", async ({
-    page,
-  }) => {
-    test.setTimeout(15000);
-    await page.fill(betInputSelector, "123");
-
-    const bettingOptions = page.locator("button.bet-btn");
-    await bettingOptions.first().click();
-
-    const signInModal = page.getByText("Please sign in to start playing!");
-
-    expect(signInModal).toBeVisible;
-  });
-
-  //   NOTE: Needs authentication/balance to pass
-  //   test("Verifies the “Max” button sets the bet to the maximum available amount that the user has on the deposit", async ({
-  //     page,
-  //   }) => {
-  //     const depositAmountSelector =
-  //       '//*[@id="empire-header"]/div[1]/div/div[3]/div[2]/div[3]/div/div/span/span/div/span[2]';
-  //     const maxDepositAmount: string | null = await page
-  //       .locator(depositAmountSelector)
-  //       .textContent();
-
-  //     await page.getByRole("button", { name: "Max" }).click();
-
-  //     await expect(page.locator(betInputSelector)).toHaveValue(
-  //       maxDepositAmount.trim(),
-  //     );
-  //   });
 });
--- utils/roulette.ts
+++ utils/roulette.ts
@@ -1,17 +1,126 @@
-import { Page } from "@playwright/test";
+import { Page, expect } from "@playwright/test";
+import { TIMEOUT } from "dns";
 
-export async function isRouletteSpinning(page: Page): Promise<boolean> {
-  return page.evaluate(() => {
-    const targetElement = document.querySelector("div.wheel");
-    if (!targetElement) {
-      return false;
+export class RoulettePage {
+  page: Page;
+  timerSelector = "div.font-numeric.text-2xl.font-bold";
+  timerVisibleSelector = "div.wheel__item--visible";
+  betInputSelector = 'input[placeholder="Enter bet amount..."]';
+  // No choice to pick it up in other way. Otherwise I would add a test ID
+  soundToggleButtonXPath =
+    '//*[@id="app"]/div[1]/div[2]/div/div/div[1]/div[2]/div/div[1]/button';
+  bettingOptionsSelector = "button.bet-btn";
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async goto() {
+    await this.page.goto("https://csgoempire.com/");
+    await expect(this.page).toHaveTitle(
+      /CSGOEmpire | The most trusted CSGO Skin Gambling Site/i,
+    );
+  }
+
+  async isRouletteSpinning(page: Page): Promise<boolean> {
+    return page.evaluate(() => {
+      const targetElement = document.querySelector("div.wheel");
+      if (!targetElement) {
+        return false;
+      }
+      const style = window.getComputedStyle(targetElement);
+      const backgroundPosition = style.backgroundPosition;
+
+      const positionValues = backgroundPosition.split(" ").map(Number);
+      const isSpinning = positionValues.some((pos) => !isNaN(pos) && pos > 0);
+
+      return isSpinning;
+    });
+  }
+
+  async verifyRouletteNotSpinning() {
+    await expect(this.page.locator(this.timerSelector)).toBeVisible();
+    const spinning = await this.isRouletteSpinning(this.page);
+    expect(spinning).toBeFalsy();
+  }
+
+  async waitForTimerToReachZero() {
+    await this.page.waitForFunction((selector: string) => {
+      const timer: Element | null = document.querySelector(selector);
+      return timer && timer.textContent?.trim() === "0.00";
+    }, this.timerSelector);
+
+    await expect(this.page.locator(this.timerVisibleSelector)).toHaveCount(1);
+  }
+
+  async toggleSound(expectedState: string[]) {
+    for (const state of expectedState) {
+      await this.page.click(this.soundToggleButtonXPath);
+      await expect(this.page.locator(this.soundToggleButtonXPath)).toHaveText(
+        state,
+      );
     }
-    const style = window.getComputedStyle(targetElement);
-    const backgroundPosition = style.backgroundPosition;
+  }
 
-    const positionValues = backgroundPosition.split(" ").map(Number);
-    const isSpinning = positionValues.some((pos) => !isNaN(pos) && pos > 0);
+  async verifyTotalWinsEquals100() {
+    const selectors = ["coin-ct", "coin-bonus", "coin-t"];
+    let totalWins = 0;
+    for (const selector of selectors) {
+      const text = await this.page.evaluate((className) => {
+        const targetElement = document.querySelector(`.${className}.h-16.w-16`);
+        const numberElement = targetElement?.nextElementSibling;
+        return numberElement ? numberElement.textContent : null;
+      }, selector);
+      totalWins += parseInt(text || "0", 10);
+    }
+    expect(totalWins).toBe(100);
+  }
 
-    return isSpinning;
-  });
+  async verifyInputReflection(amount: string) {
+    await this.page.fill(this.betInputSelector, amount);
+    await expect(this.page.locator(this.betInputSelector)).toHaveValue(amount);
+  }
+
+  async verifyButtonFunctionality(
+    buttonName: string,
+    originalValue: number,
+    expectedValue: string,
+  ) {
+    await this.page.fill(this.betInputSelector, originalValue.toString());
+    await this.page.getByRole("button", { name: buttonName }).click();
+    await expect(this.page.locator(this.betInputSelector)).toHaveValue(
+      expectedValue,
+    );
+  }
+
+  async verifyPlusOneButtonFunctionality(
+    buttonName: string,
+    originalValue: number,
+    expectedValue: string,
+  ) {
+    await this.page.fill(this.betInputSelector, originalValue.toString());
+    await this.page.getByRole("button", { name: buttonName }).first().click();
+    await expect(this.page.locator(this.betInputSelector)).toHaveValue(
+      expectedValue,
+    );
+  }
+
+  async verifyBettingOptionsAvailability() {
+    const bettingOptions = this.page.locator(this.bettingOptionsSelector);
+    await expect(bettingOptions).toHaveCount(3);
+    await bettingOptions.first().isEnabled();
+    await bettingOptions.nth(1).isEnabled();
+    await bettingOptions.nth(2).isEnabled();
+  }
+
+  async verifySignInModalOnBet() {
+    await this.page.waitForLoadState("networkidle");
+
+    await this.page.fill(this.betInputSelector, "123");
+    const customSelector = `button[disable="false"].bet-btn`;
+    await this.page.waitForSelector(customSelector);
+    await this.page.locator(customSelector).first().click();
+    const signInModal = this.page.getByText("Please sign in to start playing!");
+    expect(signInModal).toBeVisible();
+  }
 }
